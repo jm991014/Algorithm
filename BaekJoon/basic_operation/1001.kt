3d9ba898fc3f0b7cf4017658ae28c02294f5defb
Jong <jm991014@gmail.com>
2024-01-06 19:16:19 +0900

    ADD: basic operation dir
    백준 1000, 1001, 1008번 문제 업로드

---
+++ BaekJoon/basic_operation/1001.kt
@@ -0,0 +1,7 @@
+// A-B
+fun main() {
+    var input = readLine()!!.split(" ")
+    var num1 = input[0].toInt()
+    var num2 = input[1].toInt()
+    print(num1 - num2)
+}
---
+++ BaekJoon/basic_operation/1008.kt
@@ -0,0 +1,7 @@
+// A/B
+fun main() {
+    var input = readLine()!!.split(" ")
+    var num1 = input[0].toDouble()
+    var num2 = input[1].toDouble()
+    print(num1/num2)
+}
