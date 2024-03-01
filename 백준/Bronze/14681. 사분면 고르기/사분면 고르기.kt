816d08b8c3e2780cd54fbdf46e1e209350e624aa
김종명 <jm991014@ajou.ac.kr>
2024-03-02 01:21:54 +0900

    [Bronze V] Title: 사분면 고르기, Time: 92 ms, Memory: 12324 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val x = readln().toInt()
+    val y = readln().toInt()
+    when {
+        (x > 0 && y > 0) -> print("1")
+        (x < 0 && y > 0) -> print("2")
+        (x < 0 && y < 0) -> print("3")
+        (x > 0 && y < 0) -> print("4")
+    }
+}
