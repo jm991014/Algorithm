f83b56aa15e7cb9cecdf081881f377316ea24af2
김종명 <jm991014@ajou.ac.kr>
2024-03-05 01:11:44 +0900

    [Bronze V] Title: 사칙연산, Time: 120 ms, Memory: 14584 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val input = readLine()!!.split(" ")
+    var num1 = input[0].toInt()
+    var num2 = input[1].toInt()
+    println(num1 + num2)
+    println(num1 - num2)
+    println(num1 * num2)
+    println(num1 / num2)
+    println(num1 % num2)
+}
