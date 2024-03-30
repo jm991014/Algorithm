4e27684780ad9aab031a75438cc541e3cc135fa4
김종명 <jm991014@ajou.ac.kr>
2024-03-30 19:33:56 +0900

    [level 0] Title: ad 제거하기, Time: 7.11 ms, Memory: 62.3 MB -BaekjoonHub

--- 프로그래머스/0/181870. ad 제거하기/ad 제거하기.kt
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.kt
@@ -1,10 +1,4 @@
 class Solution {
-    fun solution(strArr: Array<String>): Array<String> {
-        var answer: Array<String> = arrayOf<String>()
-        for(i in strArr.indices) {
-            if(strArr[i].contains("ad"))
-            else answer += strArr[i]
-        }
-        return answer
-    }
+    fun solution(strArr: Array<String>) =
+        strArr.filter { !it.contains("ad") }
 }
