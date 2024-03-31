9c0bbdc9ff9561db773060ef051a5668617651b6
김종명 <jm991014@ajou.ac.kr>
2024-03-31 21:52:21 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 61.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.kt
@@ -0,0 +1,7 @@
+class Solution {
+    fun solution(arr: IntArray): Double {
+        var answer: Double = 0.0
+        for(i in arr.indices) answer += arr[i] 
+        return answer / arr.size
+    }
+}
