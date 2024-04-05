256f9de2d1a26797e8bc41694ee005541e86ba61
김종명 <jm991014@ajou.ac.kr>
2024-04-05 17:20:40 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.08 ms, Memory: 62.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.kt
@@ -0,0 +1,8 @@
+import kotlin.math.*
+class Solution {
+    fun solution(a: Int, b: Int): Long {
+        var answer: Long = 0
+        for(i in min(a, b)..max(a, b)) answer += i.toLong()
+        return answer
+    }
+}
