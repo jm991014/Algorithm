cd1c25c2bddc252f1e45ceac7d8cf344bf002ffd
김종명 <jm991014@ajou.ac.kr>
2024-06-14 18:58:48 +0900

    Time: 261 ms (24.36%), Space: 38 MB (75.64%) - LeetHub

--- LeetCode/0001-two-sum/0001-two-sum.kt
+++ LeetCode/0001-two-sum/0001-two-sum.kt
@@ -1,14 +1,12 @@
 class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {
-        val ans = IntArray(2)
         for(i in nums.indices) {
             for(j in i + 1..nums.size - 1) {
                 if(nums[i] + nums[j] == target) {
-                    ans[0] = i
-                    ans[1] = j
+                    return intArrayOf(i, j)
                 }
             }
         }
-        return ans
+        throw IllegalArgumentException("no answer")
     }
 }
