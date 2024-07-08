c1e1c86be6e74b6154026a7526c51bef54c0a334
김종명 <jm991014@ajou.ac.kr>
2024-07-08 21:42:10 +0900

    [level 1] Title: K번째수, Time: 22.80 ms, Memory: 64.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.kt
+++ 프로그래머스/1/42748. K번째수/K번째수.kt
@@ -1,7 +1,7 @@
 class Solution {
     fun solution(array: IntArray, commands: Array<IntArray>): IntArray {
         return commands.map { command ->
-            array.slice(IntRange(command[0] - 1, command[1] - 1)).sorted()[command[2] - 1]
+            array.slice(command[0]-1..command[1]-1).sorted()[command[2] - 1]
         }.toIntArray()
     }
 }
