ee01830486b13e30320f8d3c20e047e0791e1891
김종명 <jm991014@ajou.ac.kr>
2024-07-21 05:24:41 +0900

    Time: 116 ms (96.37%), Space: 33.7 MB (49.05%) - LeetHub

--- LeetCode/1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.kt
+++ LeetCode/1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.kt
@@ -1,14 +1,7 @@
 class Solution {
     fun findTheWinner(n: Int, k: Int): Int {
-        val friendList = mutableListOf<Int>()
-        for(i in 1..n) friendList.add(i)
-
-        var startIndex = 0
-        while(friendList.size > 1) {
-            val removeIndex = (startIndex + k - 1) % friendList.size
-            friendList.removeAt(removeIndex)
-            startIndex = removeIndex
-        }
-        return friendList[0]
+        if(n == 1) return 1
+        val ans = (findTheWinner(n - 1, k) + k) % n
+        return if (ans == 0) n else ans
     }
 }
