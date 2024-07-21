140c58298deac44fd6853f6f61e18049e5e8ef10
김종명 <jm991014@ajou.ac.kr>
2024-07-21 20:38:50 +0900

    [level 0] Title: 배열의 평균값, Time: 15.41 ms, Memory: 62.8 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.kt
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.kt
@@ -1,7 +1,3 @@
 class Solution {
-    fun solution(numbers: IntArray): Double {
-        var answer: Double = 0.0
-        numbers.forEach { answer += it }
-        return answer / numbers.size
-    }
+    fun solution(numbers: IntArray): Double = numbers.average()
 }
