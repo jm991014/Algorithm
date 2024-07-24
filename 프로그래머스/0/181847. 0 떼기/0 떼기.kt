f6b54fd872eb017b0be0a6b5c2a60c2f9d840594
김종명 <jm991014@ajou.ac.kr>
2024-07-24 21:13:56 +0900

    [level 0] Title: 0 떼기, Time: 0.03 ms, Memory: 60.4 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.kt
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.kt
@@ -1,12 +1,3 @@
 class Solution {
-    fun solution(n_str: String): String {
-        var answer: String = ""
-        for(i in 0..n_str.length-1) {
-            if(n_str[i] != '0') {
-                answer = n_str.substring(i)
-                break
-            }  
-        }
-        return answer
-    }
+    fun solution(n_str: String): String = n_str.toInt().toString()
 }
