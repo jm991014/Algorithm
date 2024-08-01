f03926639f09f73827baf978bc672ab62c78d27a
김종명 <jm991014@ajou.ac.kr>
2024-08-01 22:46:27 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 28.52 ms, Memory: 62.1 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.kt
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.kt
@@ -1,6 +1,5 @@
 class Solution {
     fun solution(s: String): String {
-        var answer = ""
         val arr = s.split(" ").map { it.toInt() }
         return "${arr.minOf{it}} ${arr.maxOf{it}}"
     }
