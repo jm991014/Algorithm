4fa414f9013d071105143d1917bd8b9bccc80263
김종명 <jm991014@ajou.ac.kr>
2024-08-24 02:11:48 +0900

    [level 3] Title: 베스트앨범, Time: 28.61 ms, Memory: 63.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun solution(genres: Array<String>, plays: IntArray): IntArray {
+        var answer = mutableListOf<Int>()
+        var genresHashMap = HashMap<String, MutableList<Pair<Int, Int>>>()
+        
+        genres.forEachIndexed { index, genre ->
+            if(genre in genresHashMap) genresHashMap[genre]!!.add(Pair(plays[index], index))
+            else genresHashMap[genre] = mutableListOf(Pair(plays[index], index))
+        }
+        
+        val genresList = genresHashMap.toList().sortedByDescending { 
+            it.second.sumOf { it.first } 
+        }.map { it.second.sortedByDescending { it.first }.take(2).map { it.second } }.flatten()
+        return genresList.toIntArray()
+    }
+}
