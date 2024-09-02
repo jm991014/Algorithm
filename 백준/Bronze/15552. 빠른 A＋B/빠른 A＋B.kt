a3046ca3a29d8fd44431c53f93d10f3fa5273c90
김종명 <jm991014@ajou.ac.kr>
2024-09-02 20:14:41 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 744 ms, Memory: 291032 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.kt
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.kt
@@ -1,12 +1,8 @@
-import java.io.BufferedReader
-import java.io.BufferedWriter
-import java.io.InputStreamReader
-import java.io.OutputStreamWriter
 import java.util.StringTokenizer
 
 fun main() {
-    val br = BufferedReader(InputStreamReader(System.`in`))
-    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val br = System.`in`.bufferedReader()
+    val bw = System.`out`.bufferedWriter()
     repeat(br.readLine().toInt()) {
         val token = StringTokenizer(br.readLine())
         val sum = (token.nextToken().toInt() + token.nextToken().toInt()).toString()
