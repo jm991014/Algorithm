08328860d2ed7bf32c427346d23d63e65c54d468
김종명 <jm991014@ajou.ac.kr>
2024-10-29 21:53:28 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 82.79 ms, Memory: 87.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.kt
@@ -0,0 +1,28 @@
+class Solution {
+    fun solution(gems: Array<String>): IntArray {
+        var answer = intArrayOf()
+        val totalGems = gems.toSet().size
+        val gemMap = mutableMapOf<String, Int>()
+        var start = 0
+        var end = 0
+        var dist = 100000
+        
+        while(true) {
+            if(gemMap.size == totalGems) {
+                if(end - start < dist) {
+                    dist = end - start
+                    answer = intArrayOf(start + 1, end)
+                }
+                if(gemMap[gems[start]] == start + 1) {
+                    gemMap.remove(gems[start])
+                }
+                start++
+            } else if(end == gems.size) {
+                break
+            } else {
+                gemMap[gems[end]] = ++end
+            }
+        }
+        return answer
+    }    
+}
