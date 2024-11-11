84d610ed96067edcce8980e975ec51e724b5a982
김종명 <jm991014@ajou.ac.kr>
2024-11-11 11:43:13 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 16.88 ms, Memory: 61.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.kt
@@ -0,0 +1,44 @@
+import kotlin.math.abs
+
+class Solution {
+    fun solution(numbers: IntArray, hand: String): String {
+        var answer = ""
+        var left = 10
+        var right = 12
+        numbers.forEach { number ->
+            when(number) {
+                1, 4, 7 -> {
+                    answer += 'L'
+                    left = number
+                }
+                3, 6, 9 -> {
+                    answer += 'R'
+                    right = number
+                }
+                else -> {
+                    val number = if(number == 0) 11 else number
+                    val lDist = abs((left - number) / 3) + abs((left - number) % 3)
+                    val rDist = abs((right - number) / 3) + abs((right - number) % 3)
+                    if (lDist == rDist) {
+                        if (hand.startsWith('l')) {
+                            answer += 'L'
+                            left = number
+                        } else {
+                            answer += 'R'
+                            right = number
+                        }
+                    } else {
+                        if (lDist < rDist) {
+                            answer += 'L'
+                            left = number
+                        } else {
+                            answer += 'R'
+                            right = number
+                        }
+                    }
+                }
+            }
+        }
+        return answer
+    }
+}
