b9abc14dfc0e68b2b4e63fe9bd8b022fdd284cb1
김종명 <jm991014@ajou.ac.kr>
2024-11-21 12:38:10 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 36.70 ms, Memory: 63 MB -BaekjoonHub

---
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.kt
@@ -0,0 +1,18 @@
+import kotlin.math.abs
+
+class Solution {
+    fun solution(survey: Array<String>, choices: IntArray): String {
+        var answer: String = ""
+        val personality = arrayOf('R', 'T', 'C', 'F', 'J', 'M', 'A', 'N')
+        val p_score = intArrayOf(0, 0, 0, 0, 0, 0, 0, 0)
+        
+        survey.forEachIndexed { idx, _ ->
+            p_score[personality.indexOf(survey[idx][1])] += choices[idx] - 4
+        }
+        
+        for(i in 0..7 step 2) {
+            answer += if (p_score[i] >= p_score[i + 1]) personality[i] else personality[i + 1]
+        }
+        return answer
+    }
+}
