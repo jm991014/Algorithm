90ce21600d682a5021d8947b7e102cf1c94b609b
김종명 <jm991014@ajou.ac.kr>
2025-01-31 22:17:20 +0900

    [level 1] Title: 신고 결과 받기, Time: 310.23 ms, Memory: 170 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.kt
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.kt
@@ -1,23 +1,13 @@
 class Solution {
-    fun solution(id_list: Array<String>, report: Array<String>, k: Int): IntArray {
-        val answer = IntArray(id_list.size)
-        val reportSet = report.toSet()
-        val reportedCount = HashMap<String, Int>()
-        val reportUser = HashMap<String, Set<String>>()
-
-        reportSet.forEach {
-            val (reporter, reported) = it.split(" ")
-            reportedCount[reported] = reportedCount.getOrDefault(reported, 0) + 1
-            reportUser[reporter] = reportUser.getOrDefault(reporter, emptySet()) + reported
-        }
-
-        id_list.forEachIndexed { index, id ->
-            reportUser[id]?.forEach {
-                if (reportedCount.getOrDefault(it, 0) >= k) {
-                    answer[index]++
-                }
-            }
-        }
-        return answer
-    }
+    fun solution(id_list: Array<String>, report: Array<String>, k: Int) =
+        report.map { it.split(" ") }
+            .groupBy { it[1] }
+            .asSequence()
+            .map { it.value.distinct() }
+            .filter { it.size >= k }
+            .flatten()
+            .map { it[0] }
+            .groupingBy { it }
+            .eachCount()
+            .run { id_list.map { getOrDefault(it, 0) }.toIntArray() }
 }
