e8c511a896be7ef6c081a26d9b29846c0fde0130
김종명 <jm991014@ajou.ac.kr>
2025-02-12 05:38:30 +0900

    [Silver II] Title: 상자넣기, Time: 188 ms, Memory: 21940 KB -BaekjoonHub

--- 백준/Silver/1965. 상자넣기/상자넣기.kt
+++ 백준/Silver/1965. 상자넣기/상자넣기.kt
@@ -5,9 +5,22 @@ fun main() = with(System.out.bufferedWriter()) {
     val box = br.readLine().split(" ").map { it.toInt() }
     val dp = IntArray(size) { 1 }
 
-    for (i in 1 until size) for (j in 0 until i) {
+    for (i in 1 until size) for (j in 0..i) {
         if (box[i] > box[j]) dp[i] = dp[i].coerceAtLeast(dp[j] + 1)
     }
-    write("${dp.maxOf { it }}\n")
+    write("${binarySearch(box)}\n")
     close()
+}
+
+fun binarySearch(box: List<Int>): Int {
+    val lis = mutableListOf<Int>()
+
+    for (num in box) {
+        val pos = lis.binarySearch(num)
+        val insertPos = if (pos < 0) -pos - 1 else pos
+
+        if (insertPos == lis.size) lis.add(num)
+        else lis[insertPos] = num
+    }
+    return lis.size
 }
