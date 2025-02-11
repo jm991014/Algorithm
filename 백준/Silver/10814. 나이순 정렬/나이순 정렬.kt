a638e67f3e16dda89d2e662c6e8fd0d890835d05
김종명 <jm991014@ajou.ac.kr>
2025-02-11 18:46:39 +0900

    [Silver V] Title: 나이순 정렬, Time: 632 ms, Memory: 70712 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.kt
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.kt
@@ -2,9 +2,7 @@ private val br = System.`in`.bufferedReader()
 
 fun main() = with(System.out.bufferedWriter()) {
     val case = br.readLine().toInt()
-    val arr = Array(case) { br.readLine().split(" ").let { (age, name) -> Triple(age.toInt(), name, it) } }
-    arr.sortedWith(compareBy<Triple<Int, String, Int>> { it.first }.thenBy { it.third }).forEach {
-        write("${it.first} ${it.second}\n")
-    }
+    val arr = Array(case) { br.readLine().split(" ").let { (age, name) -> Pair(age.toInt(), name) } }
+    arr.sortedWith(compareBy { it.first }).forEach { write("${it.first} ${it.second}\n") }
     close()
 }
