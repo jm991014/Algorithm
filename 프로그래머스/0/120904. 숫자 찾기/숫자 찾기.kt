4aab67faace565596a4792abb86e18451d307142
김종명 <jm991014@ajou.ac.kr>
2025-02-18 17:55:34 +0900

    [level 0] Title: 숫자 찾기, Time: 20.14 ms, Memory: 63.2 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.kt
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.kt
@@ -1,3 +1,3 @@
 class Solution {
-    fun solution(num: Int, k: Int): Int = num.toString().indexOf(k.toString()).let { if (it > -1) it + 1 else -1 }
+    fun solution(num: Int, k: Int): Int = "+$num".indexOf(k.toString())
 }
