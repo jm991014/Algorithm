4e49635575b2ae4fd6e0b19eee9795e43ad9d882
김종명 <jm991014@ajou.ac.kr>
2025-02-22 09:16:35 +0900

    [level 2] Title: 문자열 압축, Time: 50.49 ms, Memory: 69.7 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.kt
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.kt
@@ -1,6 +1,5 @@
 class Solution {
     fun solution(s: String): Int {
-        var answer = 0
         val result = mutableListOf<String>()
         var min = s.length
 
@@ -8,16 +7,15 @@ class Solution {
             var count = 1
             val group = s.windowed(length, length).toMutableList()
             if (s.length % length != 0) group.add((s.takeLast(s.length % length)))
-            for (i in 0 until group.size - 1) {
-                if (group[i] == group[i + 1]) count++
+            for (i in 0 until group.size) {
+                val current = group[i]
+                val next = if (i + 1 < group.size) group[i + 1] else ""
+                if (current == next) count++
                 else {
-                    if (count > 1) result.add("$count${group[i - 1]}")
-                    else result.add(group[i])
+                    result.add(if (count > 1) "$count$current" else current)
                     count = 1
                 }
             }
-            if (count > 1) result.add("$count${group.last()}")
-            else result.add(group.last())
             min = min.coerceAtMost(result.joinToString("").length)
             result.clear()
         }
