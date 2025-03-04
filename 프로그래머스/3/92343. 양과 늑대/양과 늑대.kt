e977fd243b38041543110a79bb89ad27b7ac662b
김종명 <jm991014@ajou.ac.kr>
2025-03-04 13:42:13 +0900

    [level 3] Title: 양과 늑대, Time: 4.69 ms, Memory: 63.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.kt
@@ -0,0 +1,22 @@
+class Solution {
+    fun solution(info: IntArray, edges: Array<IntArray>): Int {
+        var answer: Int = 0
+        val visited = BooleanArray(info.size).apply { this[0] = true }
+
+        fun dfs(sheep: Int, wolf: Int) {
+            if (sheep > wolf) answer = answer.coerceAtLeast(sheep) else return
+
+            edges.forEach {
+                if (visited[it[0]] && !visited[it[1]]) {
+                    visited[it[1]] = true
+                    if (info[it[1]] == 0) dfs(sheep + 1, wolf) else dfs(sheep, wolf + 1)
+                    visited[it[1]] = false
+                }
+            }
+        }
+
+        dfs(1, 0)
+
+        return answer
+    }
+}
