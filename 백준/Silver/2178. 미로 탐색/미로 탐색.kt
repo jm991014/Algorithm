3ab8a5ea15d2da05d0559e99b346142385c09b3a
김종명 <jm991014@ajou.ac.kr>
2025-03-12 18:18:08 +0900

    [Silver I] Title: 미로 탐색, Time: 160 ms, Memory: 22644 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.kt
@@ -0,0 +1,31 @@
+import java.util.LinkedList
+
+private val br = System.`in`.bufferedReader()
+private var count = 10001
+
+fun main() = with(System.out.bufferedWriter()) {
+    val (N, M) = br.readLine().split(" ").map { it.toInt() }
+    val maze = Array(N) { br.readLine().map { it.digitToInt() }.toIntArray() }
+    bfs(maze)
+    println(count)
+}
+
+fun bfs(maze: Array<IntArray>) {
+    val directions = arrayOf(Pair(1, 0), Pair(0, -1), Pair(-1, 0), Pair(0, 1))
+    val queue = LinkedList<Triple<Int, Int, Int>>().apply { offer(Triple(0, 0, 1)) }
+    val visited = Array(maze.size) { BooleanArray(maze[0].size) }.apply { this[0][0] = true }
+
+    while (queue.isNotEmpty()) {
+        val (curX, curY, depth) = queue.poll()
+        if (curX == maze.size - 1 && curY == maze[0].size - 1) count = count.coerceAtMost(depth)
+        for (direction in directions) {
+            val dx = curX + direction.first
+            val dy = curY + direction.second
+
+            if (dx in maze.indices && dy in maze[0].indices && !visited[dx][dy] && maze[dx][dy] == 1) {
+                visited[dx][dy] = true
+                queue.offer(Triple(dx, dy, depth + 1))
+            }
+        }
+    }
+}
