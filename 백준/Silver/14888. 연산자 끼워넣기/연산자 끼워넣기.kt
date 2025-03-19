523ca2025b999990f5d52a6e973931b7c7812a9a
김종명 <jm991014@ajou.ac.kr>
2025-03-19 18:27:39 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 128 ms, Memory: 21896 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.kt
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.kt
@@ -1,58 +1,24 @@
-import java.util.LinkedList
-
 private val br = System.`in`.bufferedReader()
-private val equations = mutableListOf<String>()
 private var max = Int.MIN_VALUE
 private var min = Int.MAX_VALUE
 
 fun main() = with(System.out.bufferedWriter()) {
     val N = br.readLine().toInt()
-    val operands = br.readLine().split(" ").map { it.toInt() }
-    val operators = br.readLine().split(" ").map { it.toInt() }.toIntArray()
-    getCombination(N, operators,"")
-
-    equations.forEach {
-        val result = calculate(operands, it)
-        max = max.coerceAtLeast(result)
-        min = min.coerceAtMost(result)
-    }
+    val operands = br.readLine().split(" ").map { it.toInt() }.toIntArray()
+    val (add, sub, mul, div) = br.readLine().split(" ").map { it.toInt() }
+    dfs(1, operands[0], operands, add, sub, mul, div)
     write("$max\n$min")
     close()
 }
 
-fun getCombination(N: Int, operators: IntArray, equation: String) {
-    if (equation.length == N - 1) {
-        equations.add(equation)
+fun dfs(depth: Int, result: Int, operands: IntArray, add: Int, sub: Int, mul: Int, div: Int) {
+    if (depth == operands.size) {
+        max = result.coerceAtLeast(max)
+        min = result.coerceAtMost(min)
         return
     }
-    operators.forEachIndexed { idx, operator ->
-        if (operator != 0) {
-            operators[idx] -= 1
-            getCombination(N, operators, equation + idx)
-            operators[idx] += 1
-        }
-    }
-}
-
-fun calculate(operands: List<Int>, combination: String): Int {
-    val queue = LinkedList(operands)
-    val equation = mutableListOf<Int>()
-    var count = 0
-
-    while (queue.isNotEmpty()) {
-        val cur = queue.poll()
-        equation.add(cur)
-        if (equation.size == 2) {
-            val result = when(combination[count]) {
-                '0' -> equation[0] + equation[1]
-                '1' -> equation[0] - equation[1]
-                '2' -> equation[0] * equation[1]
-                else -> equation[0] / equation[1]
-            }
-            count++
-            equation.clear()
-            equation.add(result)
-        }
-    }
-    return equation[0]
+    if (add > 0) dfs(depth + 1, result + operands[depth], operands, add - 1, sub, mul, div)
+    if (sub > 0) dfs(depth + 1, result - operands[depth], operands, add, sub - 1, mul, div)
+    if (mul > 0) dfs(depth + 1, result * operands[depth], operands, add, sub, mul - 1, div)
+    if (div > 0) dfs(depth + 1, result / operands[depth], operands, add, sub, mul, div - 1)
 }
