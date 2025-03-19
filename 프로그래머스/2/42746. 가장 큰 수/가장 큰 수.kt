2700a7c45928a4b907073560e86d8d3acdcfc26d
김종명 <jm991014@ajou.ac.kr>
2025-03-19 16:05:15 +0900

    [level 2] Title: 가장 큰 수, Time: 164.66 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.kt
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.kt
@@ -3,7 +3,6 @@ class Solution {
         val result = numbers.map { it.toString() }
             .sortedWith { num1, num2 -> (num2 + num1).compareTo(num1 + num2) }
             .joinToString("")
-            
         return if (result[0] == '0') "0" else result
     }
 }
