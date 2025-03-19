7554f69c2c4c6fcd969fa393ef2061bbf3db65b8
김종명 <jm991014@ajou.ac.kr>
2025-03-19 15:18:17 +0900

    [level 1] Title: 최소직사각형, Time: 31.89 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.kt
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.kt
@@ -2,9 +2,11 @@ class Solution {
     fun solution(sizes: Array<IntArray>): Int {
         var width = 0
         var height = 0
-        sizes.forEach { it.sort() }
-        sizes.forEach { width = width.coerceAtLeast(it[0]) }
-        sizes.forEach { height = height.coerceAtLeast(it[1]) }
+        sizes.forEach { 
+            it.sort() 
+            width = width.coerceAtLeast(it[0])
+            height = height.coerceAtLeast(it[1])
+        }
         return (width * height)
     }
 }
