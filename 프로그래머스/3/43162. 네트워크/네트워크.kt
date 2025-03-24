56d716af81454023583cb683c508ffa3ed823862
김종명 <jm991014@ajou.ac.kr>
2025-03-24 22:24:16 +0900

    [level 3] Title: 네트워크, Time: 0.81 ms, Memory: 65.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.kt
+++ 프로그래머스/3/43162. 네트워크/네트워크.kt
@@ -1,23 +1,39 @@
-class Solution {
-    fun solution(n: Int, computers: Array<IntArray>): Int {
-        val network = Array(n + 1) { IntArray(n + 1) }
-    
-        var answer = 0
-        
-        computers.forEachIndexed { index, computer ->
-            for(i in 0 until n) {
-                network[index + 1][i + 1] = computer[i]
-            }
-        }
+import java.util.LinkedList
 
-        network.forEach { net ->
-            net.forEach {
-                print(it)
+class Solution {
+    var network = 0
+    fun solution(n: Int, computers: Array<IntArray>): Int {
+        var answer = 0
+        val graph = Array(n) { mutableListOf<Int>() }
+        val visited = BooleanArray(n)
+        
+        computers.forEachIndexed { idx, computer ->
+            computer.forEachIndexed { i, c ->
+                if (c == 1) graph[idx].add(i)
             }
         }
-        // dfs
+                
+        for (i in graph.indices) {
+            for (j in graph[i].indices) {
+                if (!visited[i]) dfs(i, graph, visited)
+            }
+        }
+        return network
+    }
+    
+    fun dfs(node: Int, graph: Array<MutableList<Int>>, visited: BooleanArray) {
+        val stack = LinkedList<Int>().apply { push(node) }
+        visited[node] = true
+        network++
         
-        
-        return answer
+        while (stack.isNotEmpty()) {
+            val cur = stack.pop()
+            for (i in graph[cur]) {
+                if (!visited[i]) {
+                    stack.push(i)
+                    visited[i] = true
+                }
+            }
+        }
     }
 }
