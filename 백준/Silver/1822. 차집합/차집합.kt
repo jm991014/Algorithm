4473e0d0ef38d0f34780089468417a2230d46c82
김종명 <jm991014@ajou.ac.kr>
2025-04-27 19:52:08 +0900

    [Silver IV] Title: 차집합, Time: 1820 ms, Memory: 408104 KB -BaekjoonHub

--- 백준/Silver/1822. 차집합/차집합.kt
+++ 백준/Silver/1822. 차집합/차집합.kt
@@ -1,11 +1,11 @@
 private val br = System.`in`.bufferedReader()
 
 fun main() = with(System.out.bufferedWriter()) {
-    val (n, m) = br.readLine().split(" ").map { it.toInt() }
+    br.readLine()
     val A = br.readLine().split(" ").map { it.toInt() }.toSortedSet()
     val B = br.readLine().split(" ").map { it.toInt() }.toSortedSet()
     val answer = A - B
-    
+
     write("${answer.size}\n${answer.joinToString(" ")}")
     close()
 }
