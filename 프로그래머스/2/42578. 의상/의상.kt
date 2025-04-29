6d212bdc30aa5b5f4c948e5596434cd160615444
김종명 <jm991014@ajou.ac.kr>
2025-04-29 13:34:54 +0900

    [level 2] Title: 의상, Time: 0.34 ms, Memory: 62.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.kt
+++ 프로그래머스/2/42578. 의상/의상.kt
@@ -1,7 +1,7 @@
 class Solution {
     fun solution(clothes: Array<Array<String>>): Int =
         clothes
-            .groupBy({ it[1] }, { it[0] })
+            .groupBy({ it[1] })
             .values
             .fold(1) { acc, items -> acc * (items.size + 1) } - 1
 }
