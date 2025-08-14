0607e85a12b072ce2fc053fd515bd18aef0feb4f
김종명 <jm991014@ajou.ac.kr>
2025-08-14 11:42:34 +0900

    [Silver II] Title: 최소 힙, Time: 280 ms, Memory: 27572 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.kt
@@ -0,0 +1,16 @@
+import java.util.PriorityQueue
+
+private val br = System.`in`.bufferedReader()
+
+fun main() = with(System.out.bufferedWriter()) {
+    val pq = PriorityQueue<Int>()
+    repeat(br.readLine().toInt()) {
+        val command = br.readLine().toInt()
+
+        if (command == 0) {
+            if (pq.isEmpty()) write("0\n")
+            else write("${pq.poll()}\n")
+        } else pq.add(command)
+    }
+    close()
+}
