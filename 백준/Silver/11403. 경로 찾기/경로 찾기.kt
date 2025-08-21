b2830ca0feab7f420e8605f3672078c163e5a787
김종명 <jm991014@ajou.ac.kr>
2025-08-21 15:35:55 +0900

    [Silver I] Title: 경로 찾기, Time: 228 ms, Memory: 26092 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.kt
@@ -0,0 +1,18 @@
+private val br = System.`in`.bufferedReader()
+
+fun main() = with(System.out.bufferedWriter()) {
+    val N = br.readLine().toInt()
+    val graph = Array(N) { br.readLine().split(" ").map { it.toInt() }.toIntArray() }
+
+    for (i in 0 until N) {
+        for (j in 0 until N) {
+            for (k in 0 until N) {
+                if (graph[j][i] == 1 && graph[i][k] == 1) graph[j][k] = 1
+            }
+        }
+    }
+    repeat(N) {
+        write("${graph[it].joinToString(" ")}\n")
+    }
+    close()
+}
