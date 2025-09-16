b2efcf81efbb296ec3c76a9dd94e9e457ea64dbe
김종명 <jm991014@ajou.ac.kr>
2025-09-16 14:07:36 +0900

    [Bronze V] Title: Who is in the middle?, Time: 108 ms, Memory: 14448 KB -BaekjoonHub

---
+++ 백준/Bronze/6840. Who is in the middle？/Who is in the middle？.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] arr = br.lines().limit(3).mapToInt(Integer::parseInt).sorted().toArray();
+        bw.write(String.valueOf(arr[1]));
+        bw.close();
+    }
+}
