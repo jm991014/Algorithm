4d54d916f7debe1570c1b274228e5041e64205b0
김종명 <jm991014@ajou.ac.kr>
2025-09-18 19:40:19 +0900

    [Silver IV] Title: 수열 정렬, Time: 128 ms, Memory: 16260 KB -BaekjoonHub

---
+++ 백준/Silver/1015. 수열 정렬/수열 정렬.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] A = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] B = A.clone();
+        Arrays.sort(B);
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (A[i] == B[j]) {
+                    bw.write(j + " ");
+                    B[j] = -1;
+                    break;
+                }
+            }
+        }
+        bw.close();
+    }
+}
