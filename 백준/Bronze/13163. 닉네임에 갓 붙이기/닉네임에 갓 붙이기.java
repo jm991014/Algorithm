4a38a047d0eb145d697f229ef7d6b2e34ba2b883
김종명 <jm991014@ajou.ac.kr>
2025-09-19 14:29:23 +0900

    [Bronze III] Title: 닉네임에 갓 붙이기, Time: 112 ms, Memory: 14464 KB -BaekjoonHub

---
+++ 백준/Bronze/13163. 닉네임에 갓 붙이기/닉네임에 갓 붙이기.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            String[] input = br.readLine().split(" ");
+            input[0] = "god";
+            bw.write(String.join("", input) + "\n");
+        }
+        bw.close();
+    }
+}
