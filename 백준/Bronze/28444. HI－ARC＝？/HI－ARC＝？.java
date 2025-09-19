90e024ba28833d49833319d94ddb71869ece3127
김종명 <jm991014@ajou.ac.kr>
2025-09-19 14:12:18 +0900

    [Bronze V] Title: HI-ARC=?, Time: 108 ms, Memory: 14676 KB -BaekjoonHub

---
+++ 백준/Bronze/28444. HI－ARC＝？/HI－ARC＝？.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        bw.write(String.valueOf(input[0] * input[1] - input[2] * input[3] * input[4]));
+        bw.close();
+    }
+}
