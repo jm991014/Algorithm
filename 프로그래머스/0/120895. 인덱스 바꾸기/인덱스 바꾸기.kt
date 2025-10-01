ad4fe961c0e9e357bdb9d5304080dac976a3e98d
김종명 <jm991014@ajou.ac.kr>
2025-10-01 15:08:01 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 14.33 ms, Memory: 65.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.kt
@@ -0,0 +1,7 @@
+class Solution {
+    fun solution(my_string: String, num1: Int, num2: Int): String = my_string.toCharArray().apply {
+        val tmp = this[num1]
+        this[num1] = this[num2]
+        this[num2] = tmp
+    }.joinToString("")
+}
