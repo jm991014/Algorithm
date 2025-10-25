20a1da04959c27b18e51803f574a4c72d2f54e13
김종명 <jm991014@ajou.ac.kr>
2025-10-26 00:24:01 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 83.29 ms, Memory: 179 MB -BaekjoonHub

---
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.kt
@@ -0,0 +1,35 @@
+class Solution {
+    fun solution(edges: Array<IntArray>): IntArray {
+        var answer: IntArray = IntArray(4)
+        val maxVal = (edges.mapNotNull { it.maxOrNull() }.maxOrNull() ?: 0) + 1
+        val inCnt = IntArray(maxVal) { 0 }
+        val outCnt = IntArray(maxVal) { 0 }
+
+        for (edge in edges) {
+            val nowOut = edge[0]
+            val nowIn = edge[1]
+            outCnt[nowOut]++
+            inCnt[nowIn]++
+        }
+
+        for (nowNode in 1 until maxVal) {
+            when {
+                inCnt[nowNode] == 0 && outCnt[nowNode] >= 2 -> {
+                    answer[0] = nowNode
+                }
+                outCnt[nowNode] == 0 && inCnt[nowNode] > 0 -> {
+                    answer[2]++
+                }
+                inCnt[nowNode] >= 2 && outCnt[nowNode] == 2 -> {
+                    answer[3]++
+                }
+            }
+        }
+
+        if (answer[0] != 0) {
+             answer[1] = outCnt[answer[0]] - (answer[2] + answer[3])
+        }
+
+        return answer
+    }
+}
