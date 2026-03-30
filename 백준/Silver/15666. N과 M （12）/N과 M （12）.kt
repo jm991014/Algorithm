30573671d43c077597dfa67c4b5b58719e17a36d
김종명 <jm991014@ajou.ac.kr>
2026-03-30 16:20:38 +0900

    [Silver II] Title: N과 M (12), Time: 168 ms, Memory: 24456 KB -BaekjoonHub

--- 백준/Silver/15666. N과 M （12）/N과 M （12）.kt
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.kt
@@ -1,30 +1,28 @@
 private val br = System.`in`.bufferedReader()
 private val bw = System.out.bufferedWriter()
-private val set = mutableSetOf<String>()
 
 fun main() {
     val (N, M) = br.readLine().split(" ").map { it.toInt() }
     val nums = br.readLine().split(" ").map { it.toInt() }.sorted().toIntArray()
 
-    for (i in nums.indices) {
-        nums.permutations(mutableListOf(), M, i)
-    }
-
-    set.forEach {
-        bw.write("${it}\n")
-    }
+    nums.permutations(IntArray(M), M, 0, 0)
     bw.close()
 }
 
-fun IntArray.permutations(list: MutableList<Int>, M: Int, start: Int) {
-    if (list.size == M) {
-        set.add(list.joinToString(" "))
+fun IntArray.permutations(arr: IntArray, M: Int, start: Int, depth: Int) {
+    if (depth == M) {
+        bw.write("${arr.joinToString(" ")}\n")
         return
     }
 
+    var prev = -1
+
     for (i in start until size) {
-        list.add(this[i])
-        permutations(list, M, i)
-        list.removeLast()
+        if (prev == this[i]) continue
+
+        arr[depth] = this[i]
+        prev = this[i]
+
+        permutations(arr, M, i, depth + 1)
     }
 }
