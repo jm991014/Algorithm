aad06c4894b5d07f2045533eeb36f527042ff1de
김종명 <jm991014@ajou.ac.kr>
2026-04-01 22:01:17 +0900

    [Bronze IV] Title: 모비스, Time: 100 ms, Memory: 14440 KB -BaekjoonHub

--- 백준/Bronze/28074. 모비스/모비스.kt
+++ 백준/Bronze/28074. 모비스/모비스.kt
@@ -2,6 +2,6 @@ private val br = System.`in`.bufferedReader()
 private val bw = System.out.bufferedWriter()
 
 fun main() {
-    if (br.readLine().toSet().containsAll(setOf('M', 'O', 'B', 'I', 'S'))) bw.write("YES") else bw.write("NO")
+    br.readLine().let { str -> if ("MOBIS".all { it in str }) bw.write("YES") else bw.write("NO") }
     bw.close()
 }
